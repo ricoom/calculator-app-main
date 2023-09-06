3eab74ab0e0465eff79b2c64d177170e4769fed0
Ricoom <ericmwaniki20@gmail.com>
2023-09-06 11:12:11 +0300

    snapshot-1

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btns=document.querySelectorAll('button')
 const disp= document.getElementById('screen');
-
+var num1=0;
+var num2=0;
+var total=0;
+var op;
 function themechange() {
    if(document.getElementById('toggle').value)  {
     document.body.classList.toggle('dark-a')
@@ -11,8 +14,97 @@ window.onload=displayNumber();
 function displayNumber() {
     btns.forEach(btn=>{
         btn.addEventListener('click',()=>{
-           disp.value+=btn.innerText
-            console.log(btn.innerText)
+            //omit operators
+            if(btn.innerText!='+' && btn.innerText!='-' && btn.innerText!='x' && btn.innerText!='/' && btn.innerText!='=' && btn.innerText!='.' &&btn.innerText!='Del' && btn.innerText!='Reset'){
+                disp.value+=btn.innerText
+            }
+           
+
+            switch (btn.innerText) {
+                case "+":
+                    num1= parseInt(disp.value);
+                    disp.value=""
+                    op='+';
+                    btn.classList.toggle('active')
+                    setTimeout(() => {
+                        btn.classList.remove('active')
+                    }, 1500);
+                   
+                    break;
+
+                    case "-":
+                        num1= parseInt(disp.value);
+                        disp.value=""
+                        op='-';
+                        btn.classList.toggle('active')
+
+                        setTimeout(() => {
+                            btn.classList.remove('active')
+                        }, 1500);
+
+                        break;
+
+                        case "x":
+                        num1= parseInt(disp.value);
+                        disp.value=""
+                        op='x';
+                        btn.classList.toggle('active')
+
+                        setTimeout(() => {
+                            btn.classList.remove('active')
+                        }, 1500);
+                        
+                        break;
+                        case "/":
+                        num1= parseInt(disp.value);
+                        disp.value=""
+                        op='/';
+                        btn.classList.toggle('active')
+
+                        setTimeout(() => {
+                            btn.classList.remove('active')
+                        }, 1500);
+                        
+                        break;
+                        
+                        case "Reset":
+                    disp.value=""
+                    
+                    break;
+                    case "Del":
+                    disp.value=""
+
+                    break;
+
+                    case "=":
+                    num2=parseInt(disp.value);
+                    if(op=='+'){                      
+                        total=num1+num2;
+                        disp.value=total
+                    }
+                   if(op=='-'){
+                      
+                        total=num1-num2;
+                        disp.value=total
+                    }
+                    if(op=='x'){
+                      
+                        total=num1*num2;
+                        disp.value=total
+                    }
+                    if(op=='/'){
+                      
+                        total=num1/num2;
+                        disp.value=total
+                    }
+
+                        break;
+            
+                default: 
+                    break;
+            }
+
+
 
         })
     })
